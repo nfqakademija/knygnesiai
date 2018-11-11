225c409b5d9e6d9ac33dd72599ff1e6f0d938f53
Tadas Kundelis <kundelist@gmail.com>
2018-11-11 02:26:24 +0200

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+import App from './App';
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={ store }>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
