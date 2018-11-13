c8030c36217ed4c35dd06043feef23805a983ba1
Tadas Kundelis <kundelist@gmail.com>
2018-11-13 09:13:20 +0200

    added react router

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import MainContent from './components/MainContent';
-// import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-          {/* <header className="header_container" alt="background" >
-          
-          <div className="header__absolute">
-          <nav className="main-navbar">
-              <ul className="meninmenu d-flex justify-content-start">
-                <li className="main-navbar__item">Home</li>
-                <li className="main-navbar__item">About</li>
-                <li className="main-navbar__item">Books</li>
-                <li className="main-navbar__item">Contacts</li>
-              </ul>
-          </nav>
-        </div>
-        </header> */}
-        <MainContent />
-      </React.Fragment>
-      
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+                <MainContent />
+            </React.Fragment>
+        );
+    }
 }
 
 
