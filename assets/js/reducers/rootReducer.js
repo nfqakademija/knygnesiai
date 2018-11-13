f4a273db45b0388a6cb563aea1b32ed2995f860e
Tadas Kundelis <kundelist@gmail.com>
2018-11-13 09:22:39 +0200

    added new reducer

--- assets/js/reducers/rootReducer.js
+++ assets/js/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import books from "./bookReducer";
 import categories from "./categoryReducer";
+import mainSearch from  "./mainSearchReducer";
 
 const rootReducer = combineReducers ({
     books,
-    categories
+    categories,
+    mainSearch
 })
 
 export default rootReducer;
