43c4bc418351b2e7de7f8d1f71e061defb19e6b0
Tadas Kundelis <kundelist@gmail.com>
2018-11-13 09:14:10 +0200

    added new api

--- src/Controller/ControllerApi/BookController.php
+++ src/Controller/ControllerApi/BookController.php
@@ -6,6 +6,7 @@ namespace App\Controller\ControllerApi;
 
 use App\Service\BookService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,30 +18,60 @@ class BookController extends Controller
 {
     /**
      * @Route("/books")
+     * 
      * @param BookService $bookService
      *
      * @return JsonResponse
      */
     public function index(BookService $bookService)
     {
-        $books = $bookService->setReturnQuery(false)->getAll();
-        foreach ($books as $book) {
-            $_book['id'] = $book->getId();
-            $_book['title'] = $book->getTitle();
-            $_book['description'] = $book->getDescription();
-            $_book['author'] = $book->getAuthor();
-            $_book['yearPublication'] = $book->getYearPublication();
-            $_book['pageCount'] = $book->getPageCount();
-            $_book['status'] = $book->getStatus();
-            $_book['likeCount'] = $book->getCountLike();
-            $_book['category'] = $book->getCategory()->getName();
-            $_book['createdAt'] = $book->getCreatedAt();
-            $_book['updatedAt'] = $book->getUpdatedAt();
-            $_book['media'] = $book->getMedia()->getFileName();
-            $_books[] = $_book;
+        $request = Request::createFromGlobals();
+        $queryString = $request->query->get('q');
+        if (!isset($queryString)) {
+            $queryString = "";
         }
+        $books = $bookService->setReturnQuery(false)->getAll();
+        $bookArray = [];
+        $length = count($books);
+        for ($i = 0; $i < $length; $i++) {
+            $book = [];
+            $title = $books[$i]->getTitle();
+            if (strlen($queryString) < 1 || stripos($title, $queryString) === 0) {
+                $book = $this->insertData($book, $books[$i]);
+                array_push($bookArray, $book);
+            }
+        }
+        return new JsonResponse($bookArray, 200);
+    }
+    
+    /**
+     * @Route("/books/{id}")
+     *
+     * @return JsonResponse
+     */
+    public function getById($id) {
+        $book = $this->getDoctrine()
+           ->getRepository("App\Entity\Book\Book")
+           ->find($id);
+        $response = $this->insertData(array(), $book);
+        return new JsonResponse($response, 200);
+    }
 
-        return new JsonResponse($_books, 200);
+    //utility function
+    public function insertData($emptyArray, $data) {
+        $emptyArray['id'] = $data->getId();
+        $emptyArray['title'] = $data->getTitle();
+        $emptyArray['description'] = $data->getDescription();
+        $emptyArray['author'] = $data->getAuthor();
+        $emptyArray['yearPublication'] = $data->getYearPublication();
+        $emptyArray['pageCount'] = $data->getPageCount();
+        $emptyArray['status'] = $data->getStatus();
+        $emptyArray['likeCount'] = $data->getCountLike();
+        $emptyArray['category'] = $data->getCategory()->getName();
+        $emptyArray['createdAt'] = $data->getCreatedAt();
+        $emptyArray['updatedAt'] = $data->getUpdatedAt();
+        $emptyArray['media'] = $data->getMedia()->getFileName();
+        return $emptyArray;
     }
 
 }
