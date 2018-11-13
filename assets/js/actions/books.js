882478be82b0637c0bf0016e52281be5e0641de7
Tadas Kundelis <kundelist@gmail.com>
2018-11-13 09:16:19 +0200

    correction

--- assets/js/actions/books.js
+++ assets/js/actions/books.js
@@ -1,29 +1,12 @@
-export const fetchBooks = () => {
-    return dispatch => {
-        fetch("http://127.0.0.1:8000/books")
-            .then(response => response.json())
-            .then(json => dispatch(loadBooks(json)));
-    }
-}
+export const loadBooksDropdown = books => ({
+    books,
+    type: "LOAD_BOOKS_DROPDOWN"
+})
 
 export const loadBooks = books => ({
     books,
     type: "LOAD_BOOKS"
 })
-
-export const uploadBook = newBook => {
-    return dispatch => {
-        fetch("http://localhost:3000/books", {
-            method: "POST", 
-            headers: {
-                "Content-Type": "application/json; charset=utf-8"
-            },
-            body: JSON.stringify(newBook)
-        })
-        .then(response => response.json())
-        .then(json => dispatch(addBook(json)));
-    }
-}
 
 export const addBook = newBook => ({
     newBook,
@@ -35,14 +18,36 @@ export const updateBooks = book => ({
     type: "UPDATE_BOOKS"
 }) 
 
+export const uploadBook = newBook => {
+    return dispatch => {
+        fetch("http://127.0.0.1:8000/books", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify(newBook)
+        })
+        .then(response => response.json())
+        .then(json => dispatch(addBook(json)));
+    }
+}
 
-// export const fetchData = (filters, search, callback) => dispatch => {
-//     let filteredMovies = filterMovies(movies, filters, search, callback);
-//     if (callback) {
-//       callback();
-//     }
-//     return dispatch({
-//       type: "FETCH_MOVIES",
-//       filteredMovies
-//     });
-//   };
+export const fetchAllBooks = () => {
+    return dispatch => {
+        fetch("http://127.0.0.1:8000/books")
+            .then(response => response.json())
+            .then(json => dispatch(loadBooks(json)));
+    }
+}
+
+export const searchBooks = (searchString) => {
+    return dispatch => {
+        fetch("http://127.0.0.1:8000/books?q=" + searchString)
+            .then(response => response.json())
+            .then(json => dispatch(loadBooksDropdown(json)));
+    }
+}
+
+
+
+
