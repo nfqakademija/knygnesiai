437ed18449bc6ccfd081389d01d01512cc262365
Tadas Kundelis <kundelist@gmail.com>
2018-11-20 14:05:08 +0200

    updated /api/books with new BookRepository method

--- src/Controller/ControllerApi/BookController.php
+++ src/Controller/ControllerApi/BookController.php
@@ -12,6 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 
+
 /**
  * Class BookController
  */
@@ -28,21 +29,19 @@ class BookController extends Controller
     {
         $request = Request::createFromGlobals();
         $queryString = $request->query->get('q');
-        if (!isset($queryString)) {
-            $queryString = "";
+        $category = $request->query->get('category');
+
+        $books = $this->getDoctrine()
+          ->getRepository("App\Entity\Book\Book")
+          ->getBooks($queryString, $category);
+        
+        $response = [];
+        
+        foreach($books as $bookData) {
+            $bookObj = $bookService->getJSONdata($bookData);
+            array_push($response, $bookObj);
         }
-        $books = $bookService->setReturnQuery(false)->getAll();
-        $bookArray = [];
-        $length = count($books);
-        for ($i = 0; $i < $length; $i++) {
-            $book = [];
-            $title = $books[$i]->getTitle();
-            if (strlen($queryString) < 1 || stripos($title, $queryString) === 0) {
-                $book = $this->insertData($book, $books[$i]);
-                array_push($bookArray, $book);
-            }
-        }
-        return new JsonResponse($bookArray, 200);
+        return new JsonResponse($response, 200);
     }
     
     /**
@@ -50,48 +49,11 @@ class BookController extends Controller
      *
      * @return JsonResponse
      */
-    public function getById($id) {
+    public function getById(BookService $bookService, int $id) {
         $book = $this->getDoctrine()
            ->getRepository("App\Entity\Book\Book")
            ->find($id);
-        $response = $this->insertData(array(), $book);
+        $response = $bookService->getJSONdata($book);
         return new JsonResponse($response, 200);
     }
-
-    /**
-     * @Route("/api/books/categories/{categoryId}")
-     * 
-     * @return JsonResponse
-     */
-    public function getByCategory($categoryId){
-
-        $books = $this->getDoctrine()
-        ->getRepository("App\Entity\Book\Book")
-        ->findByCategory($categoryId);
-        $response = array();
-        $bookObj = array();
-        foreach($books as $bookData) {
-            $bookObj = $this->insertData($bookObj, $bookData);
-            array_push($response, $bookObj);
-        }
-        return new JsonResponse($response, 200);
-    }
-
-    //utility function
-    public function insertData($emptyArray, $data) {
-        $emptyArray['id'] = $data->getId();
-        $emptyArray['title'] = $data->getTitle();
-        $emptyArray['description'] = $data->getDescription();
-        $emptyArray['author'] = $data->getAuthor();
-        $emptyArray['yearPublication'] = $data->getYearPublication();
-        $emptyArray['pageCount'] = $data->getPageCount();
-        $emptyArray['status'] = $data->getStatus();
-        $emptyArray['likeCount'] = $data->getLikeCount();
-        $emptyArray['category'] = $data->getCategory()->getName();
-        $emptyArray['createdAt'] = $data->getCreatedAt();
-        $emptyArray['updatedAt'] = $data->getUpdatedAt();
-        $emptyArray['media'] = $data->getMedia()->getFileName();
-        return $emptyArray;
-    }
-
 }
