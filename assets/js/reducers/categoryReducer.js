46ad44833b21d89f867166d859cb1bce92465885
Tadas Kundelis <kundelist@gmail.com>
2018-11-20 14:03:50 +0200

    changed categories.selected type to int

--- assets/js/reducers/categoryReducer.js
+++ assets/js/reducers/categoryReducer.js
@@ -1,16 +1,13 @@
 const initialState = {
     items: [],
-    selected: ""
+    selected: 0
 }
 
 const categoryReducer = (state = initialState, action) => {
-    console.log(action)
     switch(action.type) {
         case "LOAD_CATEGORIES":
             return {...state, items: action.categories}
         case "SET_CATEGORY":
-            // updatedSelected = state.selected.slice();
-            // updatedSelected.push(action.category); 
             return {...state, selected: action.categoryId}
         default:
             return state;
