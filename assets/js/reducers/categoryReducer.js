d0d1038542af8578c31226e28a9814cb3d0c65cc
Tadas Kundelis <kundelist@gmail.com>
2018-11-21 15:41:25 +0200

    added name parameter

--- assets/js/reducers/categoryReducer.js
+++ assets/js/reducers/categoryReducer.js
@@ -1,14 +1,16 @@
 const initialState = {
     items: [],
-    selected: 0
+    selected: 0,
+    name: ""
 }
 
 const categoryReducer = (state = initialState, action) => {
+    console.log(action)
     switch(action.type) {
         case "LOAD_CATEGORIES":
             return {...state, items: action.categories}
         case "SET_CATEGORY":
-            return {...state, selected: action.categoryId}
+            return {...state, selected: action.categoryId, name: action.name}
         default:
             return state;
     }
